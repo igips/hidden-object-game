6c20e6ccefeb3f4d4f0db2a18d6c5aa88a35bc35
igips <igor.szepietowski@gmail.com>
2022-03-04 17:54:05 +0100

    Create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import "../styles/Footer.css";
+
+function Footer() {
+	return (
+		<footer>
+			<a href="https://github.com/igips" target="_blank" rel="noreferrer">
+				<i className="fab fa-github"></i>
+			</a>
+		</footer>
+	);
+}
+
+export default Footer;
