2911f14d02b3daf950dda70b1a953a7b303c1583
igips <igor.szepietowski@gmail.com>
2022-03-04 22:34:07 +0100

    Create nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,49 @@
 import "../styles/Nav.css";
+import chief from "../img/chief.png";
+import marcus from "../img/marcus.png";
+import niko from "../img/niko.png";
+import arthur from "../img/arthur.png";
+import kratos from "../img/kratos.png";
+import nathan from "../img/nathan.png";
 
 
-function Nav() {
-    return (
-      <nav>
-         
-  
-  
-  
-  
-      </nav>
-    );
-  }
-  
-  export default Nav;
+function Nav(props) {
+
+    const pic1 = props.char === "xbox" ? chief : arthur;
+    const name1 = props.char === "xbox" ? "Master Chief" : "Arthur Morgan";
+    const pic2 = props.char === "xbox" ? marcus : kratos;
+    const name2 = props.char === "xbox" ? "Marcus Fenix" : "Kratos";
+    const pic3 = props.char === "xbox" ? niko : nathan;
+    const name3 = props.char === "xbox" ? "Niko Bellic" : "Nathan Drake";
+    
+
+
+
+
+	if (props.char === "") {
+		return <nav></nav>;
+
+	} else  {
+		return (
+			<nav>
+				<div className="char-div">
+					<img className="char-img" src={pic1} alt="" />
+					<span>{name1}</span>
+				</div>
+
+				<div className="char-div">
+					<img className="char-img" src={pic2} alt="" />
+					<span>{name2}</span>
+				</div>
+
+				<div className="char-div">
+					<img className="char-img" src={pic3} alt="" />
+					<span>{name3}</span>
+				</div>
+			</nav>
+		);
+
+	} 
+}
+
+export default Nav;
