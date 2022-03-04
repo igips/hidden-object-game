94afe6ca37fe1f2bfa83e7d310e4e97b25edc442
igips <igor.szepietowski@gmail.com>
2022-03-04 22:33:54 +0100

    Create home module

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,33 @@
 import "../styles/Home.css";
+import xbox from "../img/x.jpg";
+import ps4 from "../img/ps4.jpg";
+import {Link} from "react-router-dom";
+import { useEffect } from "react";
 
-function Home() {
+function Home(props) {
+
+    useEffect(() => {
+        props.changeNav("");
+
+    }); 
+
+
+
 	return (
         <div id="home-main">
             <div id="info">
-                
+                <div id="pick-info">
+                    <p>Select an image.</p>
+                    <p>Scroll through the image to find the correct character.</p>
+                    <p>Click the character and choose the correct name.</p>
+                    <p>You will be timed and your score will be recorded, so move fast!</p>
+                </div>
+                <div id="pick-div">
+                    <Link to="/xbox"><img className="pick-img" src={xbox} alt="" /></Link>
+                    <Link to="/ps4"> <img className="pick-img" src={ps4} alt="" /></Link>
+                   
+                </div>
+
             </div>
 
         </div>
