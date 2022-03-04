261775219ad6b87e2b9e3c8068a004ef85a470a6
igips <igor.szepietowski@gmail.com>
2022-03-04 17:54:17 +0100

    Create Home

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import "../styles/Home.css";
+
+function Home() {
+	return (
+        <div id="home-main">
+            <div id="info">
+                
+            </div>
+
+        </div>
+		
+	);
+}
+
+export default Home;
