01f4fcdfd0d8f4c8be1046bc2357d2ea54ad5d4a
igips <igor.szepietowski@gmail.com>
2022-03-04 22:33:21 +0100

    Add state and router

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,29 @@ import "../styles/App.css";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import Home from "./Home";
+import Game from "./Game";
 import {HashRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 
 function App() {
+  
+  const [char, setChar] = useState("");
+
+  function changeChar(type) {
+    setChar(type);
+  }
+
+
+
 	return (
 		<HashRouter>
-			<Nav></Nav>
+			<Nav char={char}></Nav>
 			
 				<Routes>
-					<Route path="/" element={<Home/>} />
-					
+					<Route path="/" element={<Home changeNav={changeChar}/>} />
+          <Route path="/xbox" element={<Game changeNav={changeChar} type="xbox"/>} />
+          <Route path="/ps4" element={<Game changeNav={changeChar} type="ps4"/>} />					
 				</Routes>
 		
 			<Footer></Footer>
