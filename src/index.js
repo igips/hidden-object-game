bfd87b39d2bf1b179999e41073ae381ff0a1b25a
igips <igor.szepietowski@gmail.com>
2022-03-05 17:37:06 +0100

    Add firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBxPTQUVfApMgU8HU5y9oq4GN4i3OYXWcI",
+	authDomain: "hidden-object-game-f26a8.firebaseapp.com",
+	projectId: "hidden-object-game-f26a8",
+	storageBucket: "hidden-object-game-f26a8.appspot.com",
+	messagingSenderId: "1016738120465",
+	appId: "1:1016738120465:web:3c2d56298ff3db17bd42b0",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
-
-
